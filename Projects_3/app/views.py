0831ca86a810b689031440add581ad8ae993eaf1
Rohit Chawhan <105154639+LordMelodias@users.noreply.github.com>
2025-04-22 19:26:32 +0530

    Update views.py

--- Projects_3/app/views.py
+++ Projects_3/app/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect
-from django.urls import reverse
-from rest_framework import generics, permissions
+from rest_framework import generics
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from .models import Project, Comment
 from .serializers import ProjectSerializer, CommentSerializer
 
@@ -20,39 +19,26 @@ def home(request):
     
 def project_detail(request, pk):
     project = Project.objects.get(pk=pk)
-    if request.method == 'POST' and 'content' in request.POST:
-        Comment.objects.create(
-            user=request.user if request.user.is_authenticated else None,
-            project=project,
-            content=request.POST['content']
-        )
-        return redirect('project-detail', pk=pk)
     return render(request, 'project_detail.html', {'project': project})
 
 # API Views
-class ProjectListCreateView(generics.ListCreateAPIView):
+class ProjectListCreateView(generics.ListAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-    permission_classes = [permissions.AllowAny]
+    # 
 
-class ProjectDetailView(generics.RetrieveUpdateDestroyAPIView):
+class ProjectDetailView(generics.ListAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-    permission_classes = [permissions.AllowAny]
 
 
-class CommentProjectView(generics.CreateAPIView):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.AllowAny]  # Explicitly allow anyone
-
-    def perform_create(self, serializer):
-        project = Project.objects.get(pk=self.kwargs['pk'])
-        serializer.save(
-            user=self.request.user if self.request.user.is_authenticated else None,
-            project=project
-        )
-
-    def create(self, request, *args, **kwargs):
-        response = super().create(request, *args, **kwargs)
-        return redirect('project-detail', pk=self.kwargs['pk'])
+@api_view(['POST'])
+def comment_project(request, pk):
+    print("COmment:",request.data)
+    serializer = CommentSerializer(data=request.data)
+    if serializer.is_valid():
+        project = Project.objects.get(pk=pk)
+        print("Project:",project)
+        serializer.save(project=project)
+        return redirect('project-detail', pk=pk)
+    return Response(serializer.errors, status=400)
