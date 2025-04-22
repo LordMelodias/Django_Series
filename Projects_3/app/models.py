29440d598b7f2f463823cfa376d235b7606091da
Rohit Chawhan <105154639+LordMelodias@users.noreply.github.com>
2025-04-22 19:27:17 +0530

    Update models.py

--- Projects_3/app/models.py
+++ Projects_3/app/models.py
@@ -25,7 +25,6 @@ class ProjectImage(models.Model):
 
 
 class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)  # Nullable user
     project = models.ForeignKey(Project, related_name='comments', on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
