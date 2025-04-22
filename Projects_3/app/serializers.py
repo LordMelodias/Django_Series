bedbb523816285e086624ef875a0252e336642f3
Rohit Chawhan <105154639+LordMelodias@users.noreply.github.com>
2025-04-22 19:27:57 +0530

    Update serializers.py

--- Projects_3/app/serializers.py
+++ Projects_3/app/serializers.py
@@ -7,21 +7,14 @@ class ProjectImageSerializer(serializers.ModelSerializer):
         fields = ['id', 'image']
 
 class CommentSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField(read_only=True, default=None)  # Allow null user
-
     class Meta:
         model = Comment
-        fields = ['id', 'user', 'content', 'created_at']
+        fields = ['id', 'content', 'created_at']
 
 class ProjectSerializer(serializers.ModelSerializer):
     images = ProjectImageSerializer(many=True, read_only=True)
-    likes_count = serializers.SerializerMethodField()
     comments = CommentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Project
-        fields = ['id', 'title', 'description', 'image', 'technologies_used', 
-                  'issues_faced', 'created_at', 'images', 'comments']
-
-    def get_likes_count(self, obj):
-        return obj.likes.count()
+        fields = ['id', 'title', 'description', 'image', 'technologies_used', 'issues_faced', 'created_at', 'images', 'comments']
