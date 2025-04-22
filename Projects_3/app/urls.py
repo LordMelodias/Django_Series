7ed39a8f9d8b972f16e5c94d932749e4fb87ae84
Rohit Chawhan <105154639+LordMelodias@users.noreply.github.com>
2025-04-22 19:26:48 +0530

    Update urls.py

--- Projects_3/app/urls.py
+++ Projects_3/app/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import (home, project_detail, ProjectListCreateView, 
-                    ProjectDetailView, CommentProjectView)
+from .views import home, project_detail, ProjectListCreateView, ProjectDetailView, comment_project
 
 urlpatterns = [
     # Frontend URLs
@@ -10,5 +9,5 @@ urlpatterns = [
     # API URLs
     path('projects/', ProjectListCreateView.as_view(), name='project-list'),
     path('projects/<int:pk>/api/', ProjectDetailView.as_view(), name='project-detail-api'),
-    path('projects/<int:pk>/comment/', CommentProjectView.as_view(), name='project-comment'),
+    path('projects/<int:pk>/comment/', comment_project, name='project-comment'),
 ]
