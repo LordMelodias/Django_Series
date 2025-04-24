da6d1695685732508fd481ca97048a60a3cd03df
Rohit Chawhan <105154639+LordMelodias@users.noreply.github.com>
2025-04-24 08:56:35 +0530

    Update models.py

--- Projects_3/app/models.py
+++ Projects_3/app/models.py
@@ -30,4 +30,4 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Comment by {self.user or 'Anonymous'} on {self.project}"
+        return f"Comment by {'Anonymous'} on {self.project}"
